6776dca887073d0cd0e7e13eaa30f87fcc14fbc6
Eric Heidel <ericleeheidel@yahoo.com>
2024-06-06 15:27:20 -0500

    a few exercices in...

---
+++ Controllers/BikeController.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using BiancasBikes.Data;
+using Microsoft.EntityFrameworkCore;
+using BiancasBikes.Models;
+using BiancasBikes.Models.DTOs;
+
+namespace BiancasBikes.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BikeController : ControllerBase
+{
+    private BiancasBikesDbContext _dbContext;
+
+    public BikeController(BiancasBikesDbContext context)
+    {
+        _dbContext = context;
+    }
+
+    [HttpGet]
+    [Authorize]
+    public IActionResult Get()
+    {
+        return Ok(_dbContext
+            .Bikes
+            .Include(b => b.Owner)
+            // .Select(b => new BikeDTO
+            // {
+            //     Id = b.Id,
+            //     Brand = b.Brand,
+            //     Color = b.Color,
+            //     BikeTypeId = b.BikeTypeId,
+            //     OwnerId = b.OwnerId
+            // })
+            .ToList());
+    }
+
+    [HttpGet("{id}")]
+    [Authorize]
+    public IActionResult GetById(int id)
+    {
+        Bike bike = _dbContext
+            .Bikes
+            .Include(b => b.Owner)
+            .Include(b => b.BikeType)
+            .Include(b => b.WorkOrders)
+            .SingleOrDefault(b => b.Id == id);
+
+        if (bike == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(bike);
+    }
+
+    [HttpGet("inventory")]
+    [Authorize]
+    public IActionResult Inventory()
+    {
+        int inventory = _dbContext
+            .Bikes
+            .Where(b => b.WorkOrders.Any(wo => wo.DateCompleted == null))
+            .Count();
+
+        return Ok(inventory);
+    }
+}
